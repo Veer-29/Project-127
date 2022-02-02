d032d20384efd15553c320451f4e9f2712e07e71
Veer-29 <79202817+Veer-29@users.noreply.github.com>
2022-02-03 01:31:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 song1 = "";
-song2 = "";
+song2= "";
 
+
+song1_status = "";
+song2_status = "";
+
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
@@ -8,17 +16,45 @@ function preload()
 	song2 = loadSound("Believer.mp3");
 }
 
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results);
+
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
+}
+
 function setup() {
     canvas = createCanvas(600,550);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log('poseNet is Initialized');
 }
 
 function draw() {
-	image(video, 0, 0, 600, 500);
-	}
+	image(video, 0, 0, 600, 550);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
 
+	fill("#FF0000");
+	stroke("#FF0000");
+}
 function play() {
     song1.play();
     song1.setVolume(1);
